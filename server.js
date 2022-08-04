61bfa100afedc11fce1cf747c15a944ed16b4bd7
Raquel Stankey <brv1114@yahoo.com>
2022-08-04 12:42:07 -0500

    set up handlebars installation, folder, and initial files

--- server.js
+++ server.js
@@ -6,10 +6,18 @@ const sequelize = require("./config/connection");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//handlebars template engine
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
+
+//handlebars template engine
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 // sync sequelize models to the database, then turn on the server
 app.listen(PORT, () => {

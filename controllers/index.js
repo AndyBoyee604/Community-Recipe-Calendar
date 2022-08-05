29315035f0403793bd07575474753e714317b830
AndyBoyee <n.andyvue@outlook.com>
2022-08-05 17:59:25 -0500

    fixed controllers files and server

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,13 @@
-const homeRoutes = require('./home-routes');
+const router = require('express').Router();
+const homeRoutes = require('./home-routes.js');
+
+const apiRoutes = require('./api');
 
 router.use('/', homeRoutes);
+//router.use('/api', apiRoutes);
+
+router.use((req, res) => {
+  res.status(404).end();
+});
+
+module.exports = router;

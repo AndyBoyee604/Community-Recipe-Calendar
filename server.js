54a4c834ba1dd21beee3c4ed5912abc25768a4e6
AndyBoyee <n.andyvue@outlook.com>
2022-08-05 17:23:38 -0500

    rename recipe-page to homepage

--- server.js
+++ server.js
@@ -1,72 +1,23 @@
-const path = require('path');
 const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+
 const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3306;
 
-const sequelize = require('./config/connection');
-
-const hbs = exphbs.create({});
- 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
 
-app.use(require('./controllers/'));
+// turn on routes
+app.use(routes);
 
+// turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const express = require("express");
-// const routes = require("./controllers");
-// // import sequelize connection
-// const sequelize = require("./config/connection");
-
-// const app = express();
-// const PORT = process.env.PORT || 3306;
-
-// //handlebars template engine
-// const exphbs = require("express-handlebars");
-// const hbs = exphbs.create({});
-
-// const routes = require('./controllers/');
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
-// app.use(routes);
-
-// //handlebars template engine
-// app.engine("handlebars", hbs.engine);
-// app.set("view engine", "handlebars");
-
-// // sync sequelize models to the database, then turn on the server
-// app.listen(PORT, () => {
-//   console.log(`App listening on port ${PORT}!`);
-// });
-
-
-

50886096bc2b60b59de3584e4b6b531178aeca30
unknown <n.andyvue@outlook.com>
2022-08-07 16:22:49 -0500

    fixed

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-<<<<<<< HEAD
 const homeRoutes = require('./home-routes.js');
 
 const apiRoutes = require('./api');
@@ -12,17 +11,3 @@ router.use((req, res) => {
 });
 
 module.exports = router;
-=======
-
-const apiRoutes = require('./api');
-const homeRoutes = require('./home-routes.js');
-
-router.use('/api', apiRoutes);
-router.use('/', homeRoutes);
-
-router.use((req, res) => {
-    res.status(404).end();
-});
-
-module.exports = router;
->>>>>>> 9344b44632768b9e186289598edddca3344eb6d5

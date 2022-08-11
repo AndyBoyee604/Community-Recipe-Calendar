4223dc5f566bcde44602c75fca9267896ac048cf
Raquel Stankey <brv1114@yahoo.com>
2022-08-10 22:20:54 -0500

    adding final styling to login and api page

--- public/javascript/random-recipe.js
+++ public/javascript/random-recipe.js
@@ -1,18 +1,18 @@
-const randomizeMe = document.getElementById('random-meal');
-const recipeSection = document.getElementById('recipe');
+const randomizeMe = document.getElementById("random-meal");
+const recipeSection = document.getElementById("recipe");
 
-randomizeMe.addEventListener('click', () => {
-  fetch('https://www.themealdb.com/api/json/v1/1/random.php')
-    .then(res => res.json())
-    .then(res => {
+randomizeMe.addEventListener("click", () => {
+  fetch("https://www.themealdb.com/api/json/v1/1/random.php")
+    .then((res) => res.json())
+    .then((res) => {
       createMeal(res.meals[0]);
     })
-    .catch(e => {
+    .catch((e) => {
       console.warn(e);
     });
 });
 
-const createMeal = meal => {
+const createMeal = (meal) => {
   const ingredients = [];
 
   for (let i = 1; i <= 20; i++) {
@@ -28,28 +28,35 @@ const createMeal = meal => {
   const htmlSection = `
 		<div>
 			<div>
-				<img src="${meal.strMealThumb}" alt="">
-				${meal.strCategory
-      ? `<p><strong>Category:</strong> ${meal.strCategory}</p>`
-      : ''
-    }
-				${meal.strArea ? `<p><strong>Area:</strong> ${meal.strArea}</p>` : ''}
-				${meal.strTags
-      ? `<p><strong>Tags:</strong> ${meal.strTags
-        .split(',')
-        .join(', ')}</p>`
-      : ''
-    }
-				<h5>Ingredients:</h5>
+				<img id="random-img" src="${meal.strMealThumb}" alt="">
+				${
+          meal.strCategory
+            ? `<p><strong>Category:</strong> ${meal.strCategory}</p>`
+            : ""
+        }
+				${meal.strArea ? `<p><strong>Area:</strong> ${meal.strArea}</p>` : ""}
+				${
+          meal.strTags
+            ? `<p><strong>Tags:</strong> ${meal.strTags
+                .split(",")
+                .join(", ")}</p>`
+            : ""
+        }
+        <br>
+				<p class="subtitle is-5 has-text-weight-medium has-text-grey-dark is-underlined">Recipe Ingredients:</p>
 				<ul>
-					${ingredients.map(ingredient => `<li>${ingredient}</li>`).join('')}
+					${ingredients.map((ingredient) => `<li>${ingredient}</li>`).join("")}
 				</ul>
 			</div>
+      <br>
 			<div>
+      <p class="subtitle is-5 has-text-weight-medium has-text-grey-dark is-underlined">Cooking Directions</p>
 				<h4>${meal.strMeal}</h4>
 				<p>${meal.strInstructions}</p>
 			</div>
 		</div>
+    <br>
+    <br>
 		
 	`;
   recipeSection.innerHTML = htmlSection;
